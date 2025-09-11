dc65cab6a2bf5afcbd8f755647fb078c61530027
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2025-09-11 13:01:02 -0300

    codigos de py otimizados

--- acompanhamento_consumo_ferramentas/ordenar_consumo_ferramenta_melhorado.py
+++ acompanhamento_consumo_ferramentas/ordenar_consumo_ferramenta_melhorado.py
@@ -4,23 +4,25 @@ import pandas as pd
 colunas_a_manter = {'Material' : 'Codigo',
                     'Texto breve material' : 'Descrição',
                     'Data de lançamento' : 'Data',
+                    'Hora do registro' : 'Hora',
                     'Qtd.  UM registro' : 'Quantidade',
                     'Montante em MI' : 'Valor',
                     'Centro custo' : 'Setor'
                     }
 # %%
-df_ferramentas = (pd.read_csv('consumo_ferramentas.csv')
+df_ferramentas = (pd.read_csv('base_dados_ferramenta/consumo_ferramentas.csv')
                     .rename(columns= lambda column : column.strip())
                     .rename(columns= colunas_a_manter)
                     .assign(Setor= lambda c : c['Setor'].astype(str).str[-3:],
                             Quantidade= lambda c : c['Quantidade'] * -1,
-                            Valor= lambda c : c['Valor'] * -1,
-                            Data= lambda df: pd.to_datetime(df['Data'] + ' ' + df['Hora do registro'], format='%Y-%m-%d %H:%M:%S')
+                            Valor= lambda c : c['Valor'] * -1
                             )
-                    .sort_values(by='Data', ascending= True)
+                    .sort_values(by=['Data','Hora'], ascending= True)
                     .reset_index(drop= True)
                     .loc[:,colunas_a_manter.values()]
                     )
+df_ferramentas['Valor'] = df_ferramentas['Valor'].astype(str).str.replace('.',',')
+df_ferramentas['Quantidade'] =df_ferramentas['Quantidade'].astype(int)
 df_ferramentas
 # %%
 df_ferramentas.to_csv('consumo_ferramenta_ordenado.csv',index=False)
--- acompanhamento_consumo_ferramentas/ordenar_producao_melhorado.py
+++ acompanhamento_consumo_ferramentas/ordenar_producao_melhorado.py
@@ -6,13 +6,10 @@ import os
 caminho_diretorio = 'base_dados_producao'
 todos_arquivos = glob.glob(os.path.join(caminho_diretorio,'producao*.csv'))
 todos_arquivos
-#%%
-lista_dfs = []
-for arquivo in todos_arquivos :
-    df_temp = pd.read_csv(arquivo)
-    lista_dfs.append(df_temp)
-
-df_producao = pd.concat(lista_dfs,ignore_index=True)
+# %%
+df_producao = pd.concat([pd.read_csv(f,decimal='.') for f in todos_arquivos]
+                        ,ignore_index=True
+                        )
 df_producao
 # %%
 colunas_a_manter = {'Material' : 'Peca',
@@ -28,5 +25,5 @@ df_producao = (df_producao.rename(columns= lambda c : c.strip())
                           )
 df_producao
 # %%
-df_producao.to_csv('producao_ordenado.csv',index=False, decimal=',')
+df_producao.to_csv('producao_ordenado.csv', index=False, decimal=',')
 # %%
